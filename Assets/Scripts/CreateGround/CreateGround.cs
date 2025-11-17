68515adc3684cd955904bf6d34c313afa5d90b73
kenkatou354 <kenkatou354@gmail.com>
2025-11-17 18:16:07 +0900

    [Add]バトルステージのマップを作成

---
+++ Assets/Scripts/CreateGround/CreateGround.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class CreateGround : MonoBehaviour
+{
+    [SerializeField] private GameObject groundPrefabA;
+    [SerializeField] private GameObject groundPrefabB;
+    [SerializeField] private int width = 10;     // â°
+    [SerializeField] private int height = 8;     // èc
+    [SerializeField] private float spacing = 1f; // É^ÉCÉãä‘ÇÃãóó£
+
+    private void Start()
+    {
+        CreateGrid();
+    }
+
+    private void CreateGrid()
+    {
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                GameObject prefab;
+
+                // A Ç∆ B Çåå›Ç…íuÇ≠
+                if ((x + y) % 2 == 0)
+                {
+                    prefab = groundPrefabA;
+                }
+                else
+                {
+                    prefab = groundPrefabB;
+                }
+
+                Vector3 pos = new Vector3(x * spacing, 0f, y * spacing);
+                Instantiate(prefab, pos, Quaternion.identity,transform);
+            }
+        }
+    }
+}
