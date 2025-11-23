892f1f5d259514a45b59ab3d42eaeb6b3bb9c686
kenkatou354 <kenkatou354@gmail.com>
2025-11-24 05:38:55 +0900

    [Add]マナの処理をする機能を追加

---
+++ Assets/Scripts/Manager/ManaEntity.cs
@@ -0,0 +1,60 @@
+using System;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+using Unity.Profiling.LowLevel.Unsafe;
+
+public class ManaEntity
+{
+    private int _maxCurrentMana;
+    private int _currentMana;
+    public int CurrentMana => _currentMana;
+    private int Level;
+    private const int TestNumber = 1;
+    private bool _isRunning = false;
+    public event Action<int,int> OnManaChanged;
+    
+    public void Start()
+    {
+        RunManaLoop().Forget();
+    }
+
+   
+    public ManaEntity(int currentMana,int level)
+    {
+        _maxCurrentMana = currentMana;
+        _currentMana = currentMana;
+        Level = level;
+    }
+
+    public async UniTask RunManaLoop()
+    {
+        _isRunning = true;
+        while (_isRunning)
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(1));
+            HealMana(TestNumber);
+            OnManaChanged?.Invoke(_currentMana,_maxCurrentMana);
+        }
+    }
+    public void AddMana(int mana)
+    {
+        _maxCurrentMana += mana * Level;
+    }
+
+    public void HealMana(int mana)
+    {
+        _currentMana += mana;
+        if (_maxCurrentMana < _currentMana)
+        {
+            _currentMana = _maxCurrentMana;
+            _isRunning = false;
+        }
+        
+        OnManaChanged?.Invoke(_currentMana,_maxCurrentMana);
+    }
+    public void ManaCost(int cost)
+    {
+        _currentMana -= cost;
+        OnManaChanged?.Invoke(_currentMana,_maxCurrentMana);
+    }
+}
