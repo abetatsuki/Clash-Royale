05439ccd3cc2620c41ebdbe143a2e3a0add9f4c5
kenkatou354 <kenkatou354@gmail.com>
2025-11-24 05:40:01 +0900

    [Add]プレイヤーの管理機能を追加

---
+++ Assets/Scripts/GameManager/PlayerManager.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+[DefaultExecutionOrder(-1000)]
+public class PlayerManager : MonoBehaviour
+{
+    private ManaEntity _manaEntity;
+    [SerializeField] private PlayerDataSO _playerDataSO;
+    private HealthEntity _healthEntity;
+
+    public event Action<int, int> OnManaChanged
+    {
+        add => _manaEntity.OnManaChanged += value;
+        remove => _manaEntity.OnManaChanged -= value;
+    }
+
+    private void Start()
+    {
+        Debug.Log(_manaEntity.CurrentMana);
+        _manaEntity.ManaCost(10);
+        Debug.Log(_manaEntity.CurrentMana);
+        _manaEntity.Start();
+    }
+
+    public void Awake()
+    {
+        InitializeEntities();
+    }
+
+    private void InitializeEntities()
+    {
+        if (_manaEntity == null || _healthEntity == null)
+        {
+            _manaEntity = new ManaEntity(_playerDataSO.Mana, _playerDataSO.Level);
+            _healthEntity = new HealthEntity(_playerDataSO.MaxHealth);
+        }
+    }
+}
