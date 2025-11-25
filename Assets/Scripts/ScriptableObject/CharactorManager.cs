f3651c11feb8143ddf51d2eb99050854d4e17acf
kenkatou354 <kenkatou354@gmail.com>
2025-11-26 07:17:52 +0900

    [Add]NaveMeshAgentを取得するように、アニメーションコントローラーを取得するように

--- Assets/Scripts/ScriptableObject/CharactorManager.cs
+++ Assets/Scripts/ScriptableObject/CharactorManager.cs
@@ -1,6 +1,29 @@
+using System;
+using UnityEditor.Animations;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(Rigidbody))]
 public class CharactorManager : MonoBehaviour
 {
-   [SerializeField] private CharacterStatus _characterStatus;
+    [SerializeField] private CharacterStatus _characterStatus;
+    private CharactorMover _mover;
+    private NavMeshAgent _agent;
+    private Transform _this;
+    private Transform _target;
+    private AnimatorController _animatorController;
+
+    private void Awake()
+    {
+        _animatorController = GetComponentInChildren<AnimatorController>();
+        _agent = GetComponent<NavMeshAgent>();
+        _this = transform;
+        _target = GameObject.Find("target").transform;
+        _mover = new CharactorMover(_agent, _target, _this);
+    }
+
+    private void Update()
+    {
+        _mover.Update();
+    }
 }
