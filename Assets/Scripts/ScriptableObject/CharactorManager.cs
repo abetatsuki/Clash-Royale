349c2fd6f01f796cf017b8ca80718dcdc0b3c296
kenkatou354 <kenkatou354@gmail.com>
2025-11-26 08:00:48 +0900

    [Add]healthEntityを生成する処理を追加。

--- Assets/Scripts/ScriptableObject/CharactorManager.cs
+++ Assets/Scripts/ScriptableObject/CharactorManager.cs
@@ -13,6 +13,8 @@ public class CharactorManager : MonoBehaviour
     private Transform _target;
     private Animator _animator;
     private AnimationCon _animCon;
+    private HealthEntity _healthEntity;
+    public void TakeDamage( int  damage) => _healthEntity.TakeDamage(damage);
 
     private void Awake()
     {
@@ -20,8 +22,10 @@ public class CharactorManager : MonoBehaviour
         _agent = GetComponent<NavMeshAgent>();
         _this = transform;
         _target = GameObject.Find("target").transform;
+        _healthEntity = new HealthEntity(100); 
         _mover = new CharactorMover(_agent, _target, _this);
         _animCon = new AnimationCon(_animator, _mover);
+        TakeDamage(1);
     }
 
     private void Update()
