8f477c3c16e5bd880e88d6aee7bb2f3950699ad5
kenkatou354 <kenkatou354@gmail.com>
2025-11-26 07:16:56 +0900

    [Add]RigidBodyでの移動方法をNavMeshAgentに変更。

--- Assets/Scripts/Charactor/CharactorMover.cs
+++ Assets/Scripts/Charactor/CharactorMover.cs
@@ -1,15 +1,18 @@
+using System.Data;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class CharactorMover
 {
-    private Rigidbody _rb;
+    private NavMeshAgent _agent;
     private Transform _target;
     private Transform _this;
+    private float _distance = 3f;
+    private Vector3 _lastTargetPos;
 
-    public CharactorMover(Rigidbody rb, Transform target, Transform _this)
+    public CharactorMover(NavMeshAgent agent, Transform target, Transform _this)
     {
-        _rb = rb;
+        _agent = agent;
         _target = target;
         this._this = _this;
     }
@@ -21,13 +24,22 @@ public class CharactorMover
 
     private float DistanceToTarget()
     {
-        return Vector3.Distance(_target.position, _rb.position);
+        return Vector3.Distance(_target.position, _agent.transform.position);
     }
 
     private void Move()
     {
-        float distance = DistanceToTarget();
-        Vector3 direction = (_target.position - _rb.position).normalized;
-        _rb.linearVelocity = direction * 3f;
+        if (DistanceToTarget() > _agent.stoppingDistance)
+        {
+            if (_lastTargetPos != _target.position)
+            {
+                _agent.SetDestination(_target.position);
+                _lastTargetPos = _target.position;
+            }
+        }
+        else
+        {
+            Debug.Log("最も近い距離まで歩きました。");
+        }
     }
 }
