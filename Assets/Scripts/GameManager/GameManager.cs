51f8ca063ed6a7ed8fd1c4bcf58345b60149d704
kenkatou354 <kenkatou354@gmail.com>
2025-11-26 16:43:29 +0900

    [Add]エネミーコンテナクラスを生成する処理する追加

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -5,9 +5,10 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private SpawnManager _spawnManager;
     [SerializeField] private PlayerManager  _playerManager;
-
+    private EnemyContainer _enemyContainer;
     private void Awake()
     {
         _spawnManager.SetPlayerManeger(_playerManager);
+        _enemyContainer = new EnemyContainer();
     }
 }
